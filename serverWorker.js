f5269f210bdc0f5439df190fee5802a89cfe7ab3
Cory Mortimer <cory.m.mortimer@gmail.com>
2017-01-13 01:25:44 -0800

    Add serverWorker.js file

---
+++ serverWorker.js
@@ -0,0 +1,66 @@
+var CACHE_NAME = 'tower-of-hanoi-cache-v1';
+var urlsToCache = [
+  // BOTH
+  '/',
+  '/favicon.ico',
+  // DEVELOPMENT
+  '/styles.bundle.css',
+  '/inline.bundle.js',
+  '/vendor.bundle.js',
+  '/main.bundle.js',
+  // PRODUCTION
+  'styles.cdd412ca690ab3315db3.bundle.css',
+  'inline.8a1a31feefeb02b84bfa.bundle.js',
+  'main.6970d88a1d5666d3086c.bundle.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
